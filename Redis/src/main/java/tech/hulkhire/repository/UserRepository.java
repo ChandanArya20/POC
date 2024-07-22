8ad08d17c601b21644091c94c0e358c5b656a6b2
Chandan <chandank1848@gmail.com>
2024-07-23 01:10:06 +0530

    redis poc

--- Redis/src/main/java/tech/hulkhire/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package tech.hulkhire.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import tech.hulkhire.model.User;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-}
